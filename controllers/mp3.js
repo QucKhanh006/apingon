871ed845ff4dadc49a87b6f1bfef30b651d57981
QucKhanh006 <quockhanh200555@gmail.com>
2024-10-15 23:16:12 +0700

    Update mp3.js

--- controllers/mp3.js
+++ controllers/mp3.js
@@ -3,5 +3,5 @@ const { errorHandler } = require("../utils");
 exports.mp3 = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../data/mp3.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-res.json({"Authors":"loveaivy","data":`${link}`});
+res.json({"Authors":"quoc khanh","data":`${link}`});
 };
