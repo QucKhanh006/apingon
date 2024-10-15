a02f64fc0f23bd8ca359dfd8b6d9d1217f89ab84
QucKhanh006 <quockhanh200555@gmail.com>
2024-10-15 23:15:01 +0700

    Update putin.js

--- controllers/putin.js
+++ controllers/putin.js
@@ -3,5 +3,5 @@ const { errorHandler } = require("../utils");
 exports.putin = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../data/putin.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-res.json({"Authors":"hoangphuong","data":`${link}`});
+res.json({"Authors":"quoc khanh","data":`${link}`});
 };
