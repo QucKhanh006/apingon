99ed48567268fbc13b7ae775464e86c7d5bd15d8
QucKhanh006 <quockhanh200555@gmail.com>
2024-10-15 23:18:49 +0700

    Update doraemon.js

--- controllers/doraemon.js
+++ controllers/doraemon.js
@@ -3,5 +3,5 @@ const { errorHandler } = require("../utils");
 exports.doraemon = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../data/doraemon.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-res.json({"Authors":"hoangphuong","url":`${link}`});
+res.json({"Authors":"quoc khanh","url":`${link}`});
 };
