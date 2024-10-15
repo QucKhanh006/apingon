f0968a283e98ac9438dc6ef039d2c9a6700dfcc6
QucKhanh006 <quockhanh200555@gmail.com>
2024-10-15 23:14:11 +0700

    Update robot.js

--- controllers/robot.js
+++ controllers/robot.js
@@ -3,5 +3,5 @@ const { errorHandler } = require("../utils");
 exports.robot = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../data/robot.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-  res.json({"Authors":"gia khanh","url":`${link}`});
+  res.json({"Authors":"quoc khanh","url":`${link}`});
 };
