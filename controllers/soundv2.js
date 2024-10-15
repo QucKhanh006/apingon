e6b2b87e603a418be173cc2cfcb5651f3bd0c9ac
QucKhanh006 <quockhanh200555@gmail.com>
2024-10-15 23:12:45 +0700

    Update soundv2.js

--- controllers/soundv2.js
+++ controllers/soundv2.js
@@ -3,5 +3,5 @@ const { errorHandler } = require("../utils");
 exports.soundv2 = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../data/sound.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-  res.json({"Authors":"gia Khánhh","url":`${link}`});
+  res.json({"Authors":"quoc khanh","url":`${link}`});
 };
