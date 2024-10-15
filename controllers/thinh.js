8bf33816074b74e2e22d7444005f542389e68ee2
QucKhanh006 <quockhanh200555@gmail.com>
2024-10-15 23:10:01 +0700

    Update thinh.js

--- controllers/thinh.js
+++ controllers/thinh.js
@@ -3,5 +3,5 @@ const { errorHandler } = require("../utils");
 exports.thinh = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../data/thinh.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-  res.json({"Authors":"gia khanh","data":`${link}`});
+  res.json({"Authors":"quoc khanh","data":`${link}`});
 };
