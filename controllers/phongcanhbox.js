de72fbc275fcd9707273eb3c6e98e3f2eecea13f
QucKhanh006 <quockhanh200555@gmail.com>
2024-10-15 23:15:24 +0700

    Update phongcanhbox.js

--- controllers/phongcanhbox.js
+++ controllers/phongcanhbox.js
@@ -3,5 +3,5 @@ const { errorHandler } = require("../utils");
 exports.phongcanhbox = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../data/phongcanhbox.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-res.json({"Authors":"hoangphuong","data":`${link}`});
+res.json({"Authors":"quoc khanh","data":`${link}`});
 };
