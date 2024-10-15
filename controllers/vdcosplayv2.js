e96d587071a4b81ef4b24ea85fe38b2bbaf768ba
QucKhanh006 <quockhanh200555@gmail.com>
2024-10-15 23:10:50 +0700

    Update vdcosplayv2.js

--- controllers/vdcosplayv2.js
+++ controllers/vdcosplayv2.js
@@ -3,5 +3,5 @@ const { errorHandler } = require("../utils");
 exports.vdcosplayv2 = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../data/vdcosplayv2.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-  res.json({"Authors":"loveaivy","url":`${link}`});
+  res.json({"Authors":"quoc khanh","url":`${link}`});
 };
