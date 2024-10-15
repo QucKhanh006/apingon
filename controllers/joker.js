80697fff52ca4e50a9cddc25013aaf9505abab6e
QucKhanh006 <quockhanh200555@gmail.com>
2024-10-15 23:16:25 +0700

    Update joker.js

--- controllers/joker.js
+++ controllers/joker.js
@@ -3,5 +3,5 @@ const { errorHandler } = require("../utils");
 exports.joker = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../data/joker.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-  res.json({"Authors":"gia Khánhh","data":`${link}`});
+  res.json({"Authors":"quoc khanh","data":`${link}`});
 };
