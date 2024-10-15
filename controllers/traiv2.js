38dd50fb0ae1f832746b90b44197a7b670a7e684
QucKhanh006 <quockhanh200555@gmail.com>
2024-10-15 23:11:35 +0700

    Update traiv2.js

--- controllers/traiv2.js
+++ controllers/traiv2.js
@@ -3,5 +3,5 @@ const { errorHandler } = require("../utils");
 exports.traiv2 = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../data/trai.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-  res.json({"Authors":"gia khanh","url":`${link}`});
+  res.json({"Authors":"quoc khanh","url":`${link}`});
 };
